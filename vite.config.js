4ed45d151000a002628b107d6fb05ee148022261
Daurin Gonzalez <dauringonzales6@gmail.com>
2025-03-23 15:57:05 -0400

    actualizando vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'build', 
-    chunkSizeWarningLimit: 1000 
-  }
-})
+ import react from '@vitejs/plugin-react'
+ 
+ // https://vitejs.dev/config/
+ export default defineConfig({
+   plugins: [react()],
+ })
